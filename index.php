5092c50172b5eeb6f5dc4dad1338e6783ab37855
nadiaelasakinah <56064760+nadiaelasakinah@users.noreply.github.com>
2019-12-30 12:38:44 +0700

    update

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Perpustakaan</title>
+
+	<link rel="stylesheet" href="assets\css\bootstrap.css"> 
+	<!-- <link rel=”stylesheet” href="assets\css\bootstrap.min.css"> -->
+	<script src="assets/js/jquery.js"></script> 
+	<script src="assets/js/popper.js"></script> 
+	<script src="assets/js/bootstrap.js"></script>
+
+</head>
+<body>
+  <header>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Welcome to My Web</h1>
+                    <div class="lead">Aku sedang belajar Bootstrap 4</div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </header>
+</body>
+</html>
