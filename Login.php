8791cde8023824d4bba484b61d759542b3485d3f
AminahNur17 <56065499+AminahNur17@users.noreply.github.com>
2019-12-31 12:14:52 +0700

    Login udah, tapi belum kehubung ke database, masih tampilan

---
+++ Login.php
@@ -0,0 +1,77 @@
+<?php
+session_start();//session starts here
+
+?>
+
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css"  href="bootstrap-4.4.1-dist/css/bootstrap.css">
+    <title>Login</title>
+
+<style>
+
+/*.panel-login{
+    margin-left: 75%;
+    margin-top: 200px;
+}*/
+
+body, html{
+    height: 100%;
+    margin: 0;
+    
+}
+
+.bg {
+  /* The image used */
+  background-image: url("bootstrap-4.4.1-dist/image/2.jpeg");
+
+  /* Full height */
+  height: 100%; 
+  margin: 0%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+</style>
+</head>
+<body >
+        <div class="bg">
+        <div class="container">
+            <!-- <div class="float-right"> -->
+            <div class="row">
+                <div class="col-md-4 offset-md-8" style="margin-top: 220px">
+                    <div class="login-panel panel panel-success" float-right >
+                        <div class="panel-heading" >
+
+                            <h3 class="panel-title">Sign In</h3>
+                        </div>
+                        <div class="panel-body">
+                            <form role="form" method="post" action="login.php">
+                                <fieldset>
+                                    <div class="form-group"  >
+                                        <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="form-control" placeholder="Password" name="pass" type="password" value="">
+                                    </div>
+                                        <input class="btn btn-lg btn-success btn-block" type="submit" value="login" name="login" >
+                                </fieldset>
+                            </form>
+                        </div>
+                        <div>
+                            <a href="admin_login.php">Admin</a> | 
+                            <a href="registration.php">Registrasi</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+</body>
+</html>
