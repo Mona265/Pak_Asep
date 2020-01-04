25dd6c9debc8ccee3a13ca845e70a2a14cc8c989
AminahNur17 <56065499+AminahNur17@users.noreply.github.com>
2020-01-04 22:02:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css"  href="bootstrap-4.4.1-dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css"  href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet"  href="bootstrap/css/style.css"> 
     <!-- <link rel=”stylesheet” href="assets\css\bootstrap.min.css"> -->
     <script src="bootstrap/js/jquery.js"></script> 
