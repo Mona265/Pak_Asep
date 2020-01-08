935d34faf90bf59d62c8bb37bda4775a2e194a44
Mona265 <56064655+Mona265@users.noreply.github.com>
2020-01-08 20:21:05 +0700

    pull

--- about.php
+++ about.php
@@ -1,37 +1,212 @@
+<!DOCTYPE html>
+<!-- Template by quackit.com -->
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>About us</title>
+    <style type="text/css">
+
+        /* Layout */
+        body {
+            min-width: 630px;
+        }
+
+        #container {
+            padding-left: 200px;
+            padding-right: 190px;
+        }
+        
+        #container .column {
+            position: relative;
+            float: left;
+        }
+        
+        #center {
+            padding: 10px 20px;
+            width: 100%;
+        }
+        
+        #left {
+            width: 180px;
+            padding: 0 10px;
+            right: 240px;
+            margin-left: -100%;
+        }
+        
+        #right {
+            width: 130px;
+            padding: 0 10px;
+            margin-right: -100%;
+        }
+        
+        #footer {
+            clear: both;
+        }
+        
+        /* IE hack */
+        * html #left {
+            left: 150px;
+        }
+
+        /* Make the columns the same height as each other */
+        #container {
+            overflow: hidden;
+        }
+
+        #container .column {
+            padding-bottom: 1001em;
+            margin-bottom: -1000em;
+        }
+
+        /* Fix for the footer */
+        * html body {
+            overflow: hidden;
+        }
+        
+        * html #footer-wrapper {
+            float: left;
+            position: relative;
+            width: 100%;
+            padding-bottom: 10010px;
+            margin-bottom: -10000px;
+            background: #fff;
+        }
+
+        /* Aesthetics */
+        body {
+            margin: 0;
+            padding: 0;
+            font-family:Sans-serif;
+            line-height: 1.5em;
+        }
+        
+        p {
+            color: #555;
+        }
+
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+        
+        nav ul a {
+            color: darkgreen;
+            text-decoration: none;
+        }
+
+        #header, #footer {
+            font-size: large;
+            padding: 0.3em;
+            background: #BCCE98;
+        }
+
+        #left {
+            background: #DAE9BC;
+        }
+        
+        #right {
+            background: #F7FDEB;
+        }
+
+        #center {
+            background: #fff;
+        }
+
+        #container .column {
+            padding-top: 1em;
+        }
+        
+    </style>
+    
+    </head>
+
+<body>
+
+    <header id="header"><p>Header...</p></header>
+
+    <div id="container">
+
+        <main id="center" class="column">
+            <article>
+            
+                <h1>Heading</h1>
+                <p><script>generateText(50)</script></p>
+            
+            </article>                              
+        </main>
+
+        <nav id="left" class="column">
+            <h3>Left heading</h3>
+            <ul>
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+                <li><a href="#">Link 5</a></li>
+            </ul>
+            <h3>Left heading</h3>
+            <ul>
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+                <li><a href="#">Link 5</a></li>
+            </ul>
+
+        </nav>
+
+        <div id="right" class="column">
+            <h3>Right heading</h3>
+            <p><script>generateText(1)</script></p>
+        </div>
+
+    </div>
+
+    <div id="footer-wrapper">
+        <footer id="footer"><p>Footer...</p></footer>
+    </div>
+
+</body>
+
+</html>
+
+
+<!-- 
 <?php
 // Data 1
 $about [] = array(
     
-    'nim'    => 4617010059,
-    'nama'   => 'Nadia Ela Sakina ',
-    'kelas' => 'TI 5A',
-    'Matkul' => 'WEB 2',
-    'jurusan' => 'TIK'
+    // 'nim'    => 4617010059,
+    // 'nama'   => 'Nadia Ela Sakina ',
+    // 'kelas' => 'TI 5A',
+    // 'Matkul' => 'WEB 2',
+    // 'jurusan' => 'TIK'
 );
 
 // Data 2
 $about [] = array(
 
-    'nim'    => 4617010011,
-    'nama'   => 'Nur Aminah',
-    'kelas' => 'TI 5A',
-    'Matkul' => 'WEB 2',
-    'jurusan' => 'TIK'
+    // 'nim'    => 4617010011,
+    // 'nama'   => 'Nur Aminah',
+    // 'kelas' => 'TI 5A',
+    // 'Matkul' => 'WEB 2',
+    // 'jurusan' => 'TIK'
   
 );
 
 // Data 3
 $about [] = array(
   
-    'nim'    => 4617010021,
-    'nama'   => 'Ramona Matovani',
-    'kelas' => 'TI 5A',
-    'Matkul' => 'WEB 2',
-    'jurusan' => 'TIK'
+    // 'nim'    => 4617010021,
+    // 'nama'   => 'Ramona Matovani',
+    // 'kelas' => 'TI 5A',
+    // 'Matkul' => 'WEB 2',
+    // 'jurusan' => 'TIK'
 );
 
 // Mengencode mswh menjadi json
-$jsonfile = json_encode($about, JSON_PRETTY_PRINT);
+// $jsonfile = json_encode($about, JSON_PRETTY_PRINT);
 
 // Menyimpan msh ke dalam anggota.json
-file_put_contents('anggota.json', $jsonfile);
+// file_put_contents('anggota.json', $jsonfile); 
--- donasi.php
+++ donasi.php
@@ -7,6 +7,33 @@ include 'koneksi.php';
 <head>
   <title>Kamubisa.com</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->  
+    <link rel="icon" type="image/png" href="bootstrap/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="bootstrap/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="bootstrap/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="bootstrap/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="bootstrap/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="bootstrap/vendor/animate/animate.css">
+<!--===============================================================================================-->  
+    <link rel="stylesheet" type="text/css" href="bootstrap/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="bootstrap/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="bootstrap/vendor/select2/select2.min.css">
+<!--===============================================================================================-->  
+    <link rel="stylesheet" type="text/css" href="bootstrap/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/util.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/main.css">
+<!--===============================================================================================-->
   
 </head>
 <body>
@@ -70,6 +97,7 @@ include 'koneksi.php';
 
                 <a href="editdonasi.php?id=<?php echo $d['id']; ?>">EDIT</a>
                 <a href="delete.php?id=<?php echo $d['id']; ?>" onclick="return confirm('Hapus Data?');">HAPUS</a>
+                <a href="donatur.php">Donasi</a>
             </td>
         </tr>
         <?php
@@ -79,8 +107,8 @@ include 'koneksi.php';
       <tbody>
     </table>
     </div>
-  </div>
 
+   
   
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
